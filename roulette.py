47a63e2d8411a736820abb1a0cae91e38fdeac5f
Dushyanth Chowdary <dushyanth.chowdary@am.sony.com>
2018-05-08 15:41:44 -0400

    attempting to add threading

--- roulette.py
+++ roulette.py
@@ -2,6 +2,9 @@ import random
 import dns.reversename
 import argparse
 import nmap
+import socket
+import threading
+import time
 
 class bcolors:
     HEADER = '\033[95m'
@@ -45,28 +48,49 @@ def check_tcp(ip, p):
 		else:
 			return False
 
+
+def thread(args,n):
+
+	ip,port = generate_ip_port(args.port)
+	print n
+
+	if args.dns:
+		dns = get_dns(ip)
+
+	succ = check_tcp(ip, port)
+	if args.open == True:
+		if succ == True:
+			print "{0}[:{1}]".format(ip,port) 
+			print "TCP connection status - " + bcolors.OKGREEN + "Success" + bcolors.ENDC
+	else:
+		print "{0}[:{1}]".format(ip,port) 
+		if succ == True:
+			print "TCP connection status - " + bcolors.OKGREEN + "Success" + bcolors.ENDC
+		else:
+			print "TCP connection status - " + bcolors.FAIL + "FAIL" + bcolors.ENDC
+
+	if succ == True and args.communicate == True:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.settimeout(5.0)
+		try:
+			s.connect((ip, int(port)))
+			data = s.recv(1024)
+			if data != None:
+				print "Recieved data - {0}".format(repr(data))
+			s.close()
+		except:
+			print bcolors.OKBLUE + "Unable to connect using python sockets" + bcolors.ENDC
+
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Identify services on the internet.')
 	parser.add_argument('--dns', action='store_const', const=True, help='resolve DNS query')
 	parser.add_argument('--port', metavar='p', type=str, nargs='?', help='set static ip')
 	parser.add_argument('--open', action='store_const', const=True, help='print only if port is OPEN')
 	parser.add_argument('--loop', metavar='lp', type=int, nargs='?', default=1, help='Loop n times')
+	parser.add_argument('--communicate', action='store_const', const=True, help='try to communicate with port if OPEN')
+
 	args = parser.parse_args()
 
 	for n in xrange(args.loop):
-		ip,port = generate_ip_port(args.port)
-
-		if args.dns:
-			dns = get_dns(ip)
-
-		succ = check_tcp(ip, port)
-		if args.open == True:
-			if succ == True:
-				print "{0}[:{1}]".format(ip,port) 
-				print "TCP connection status - " + bcolors.OKGREEN + "Success" + bcolors.ENDC
-		else:
-			print "{0}[:{1}]".format(ip,port) 
-			if succ == True:
-				print "TCP connection status - " + bcolors.OKGREEN + "Success" + bcolors.ENDC
-			else:
-				print "TCP connection status - " + bcolors.FAIL + "FAIL" + bcolors.ENDC
+		threading.Thread(target=thread(args,n)).start()
